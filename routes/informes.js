8c533d3ec70844cf6125e8b580551728e15ec781
Naza <nazadoto@gmail.com>
2025-08-21 12:40:04 -0300

    ok

--- routes/informes.js
+++ routes/informes.js
@@ -1,5 +1,5 @@
-import express from "express";
-import db from "../db.js"; // tu conexión a la DB
+const express = require('express');
+const db = require("../db.js"); // tu conexión a la DB
 
 const router = express.Router();
 
